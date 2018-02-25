7fd9e2696c165bc31e9f84cad7c27cd6575fdcc2
ImDaveead <ImDaveead@users.noreply.github.com>
2018-02-25 10:18:46 -0500

    removed colors, and fixed package.json

--- bot.js
+++ bot.js
@@ -4,8 +4,6 @@ const mysql = require("./modules/mysql.js")
 const fs = require("fs"); // Idk this is good for loading json files.
 const config = JSON.parse(fs.readFileSync('config.json'))
 
-require("colors"); // For example, ("This is red.".red)
-
 var mysqluser = process.env.mysqluser;
 var mysqlpass = process.env.mysqlpass;
 var mysqlhost = process.env.mysqlhost;
