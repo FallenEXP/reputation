aa504978dbf89dce815301619a19d379fb841aed
ImDaveead <ImDaveead@users.noreply.github.com>
2018-02-25 18:22:50 -0500

    fix syntax

--- modules/reputation/roleHandler.mod.js
+++ modules/reputation/roleHandler.mod.js
@@ -5,19 +5,20 @@ exports.dependencies = [];
 exports.load = function() {
 	var tinygradient = require("tinygradient")
 
-	var names = tinygradient({color: '#'+api.config.colors.lotsOfNegRep, pos: 0},
-	{color: '#'+api.config.colors.noRep, pos: 0.5},
-	{color: '#'+api.config.colors.lotsOfRep, pos: 1})
+	var names = tinygradient(
+		{color: '#'+api.config.colors.lotsOfNegRep, pos: 0},
+		{color: '#'+api.config.colors.noRep, pos: 0.5},
+		{color: '#'+api.config.colors.lotsOfRep, pos: 1});
 
-	var colors = names.rgb(api.config.numOfColorRoles)
+	var colors = names.rgb(api.config.numOfColorRoles);
 
 	var output = [];
 
 	colors.forEach( function(obj) {
 	  output.push(Math.floor(obj["_r"])*Math.floor(obj["_g"])*Math.floor(obj["_b"]))
-	})
-	
+	});
+
 	return {
-		output: return JSON.stringify(output);
+		output: return JSON.stringify(output)
 	}
 }
