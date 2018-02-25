e7a062cd7c7f2376464a7604e2292f730ddf797c
Nathan Alex <nathanalex66@gmail.com>
2018-02-24 22:08:30 -0500

    Base Bot File
    This is just the base for the bot.

---
+++ bot.js
@@ -0,0 +1,15 @@
+/////////////////////
+// Simple Bot Base //
+/////////////////////
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs"); // Idk this is good for loading json files.
+const config = JSON.parse(fs.readFileSync('config.json'))
+
+require("colors"); // For example, ("This is red.".red)
+
+client.login();
+
+client.on('ready', () => {
+  console.log((`[BOT] Logged in as ${client.user.tag}!`).green);
+});
