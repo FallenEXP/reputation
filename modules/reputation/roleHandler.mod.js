618843d6fd4e42827ffa60ebe01e809be2cbed97
filip9696 <filip@kinmails.com>
2018-02-25 17:00:17 -0500

    Made gradient generator

--- modules/reputation/roleHandler.mod.js
+++ modules/reputation/roleHandler.mod.js
@@ -1,7 +1,18 @@
+const tinygradient = require("tinygradient")
 exports.modID = "roleHandler";
 // Array of other modules that are needed to be loaded BEFORE this one.
 exports.dependencies = [];
 exports.load = function() {
+	var tinygradient = require("tinygradient")
 
-	return "what api.roleHandler will return"
+	var names = tinygradient({color: '#C0392B', pos: 0}, {color: '#F1C40F', pos: 0.5}, {color: '#2ECC71', pos: 1})
+
+	var colors = names.rgb(40)
+
+	var output = [];
+
+	colors.forEach( function(obj) {
+	  output.push(Math.floor(obj["_r"])*Math.floor(obj["_g"])*Math.floor(obj["_b"]))
+	})
+	return output
 }
