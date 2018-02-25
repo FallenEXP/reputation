1f57ec23bba3bdd1aa2f00d8e1c7575dc93dae61
filip9696 <filip@kinmails.com>
2018-02-25 10:04:47 -0500

    Added basic functions

--- modules/mysql.js
+++ modules/mysql.js
@@ -1,2 +1,34 @@
+var mysql = require("mysql")
+
+var connection;
+
 exports.connect = function(user, pass, host, db) {
+  conn = mysql.createConnection({
+    host     : host,
+    user     : user,
+    password : pass,
+    database : db
+  });
+  conn.connect()
+  conn.query('SELECT `id` FROM `userdata`', function (error, results, fields) {
+    if (error) {
+      return error
+    } else {
+      return true
+    }
+  })
+}
+
+exports.close = function() {
+  conn.end()
+}
+
+exports.query = function(query) {
+  conn.query(query, function (error, results, fields) {
+    if (error) {
+      return error
+    } else {
+      return results
+    }
+  })
 }
