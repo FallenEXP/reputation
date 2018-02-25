566e3d4874753bd0df5092827c8a4254152422b6
ImDaveead <ImDaveead@users.noreply.github.com>
2018-02-25 10:34:44 -0500

    changed some stuffs

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-const mysql = require("mysql")
-const db = require("./modules/mysql.js")
-const fs = require("fs"); // Idk this is good for loading json files.
+const db = require("./modules/mysql.js");
+const fs = require("fs");
 const config = JSON.parse(fs.readFileSync('config.json'))
 
+const mysql = require("mysql");
 var mysqluser = process.env.mysqluser;
 var mysqlpass = process.env.mysqlpass;
 var mysqlhost = process.env.mysqlhost;
@@ -12,24 +12,33 @@ var mysqldb = process.env.mysqldb;
 
 console.log(mysql.connect(mysqluser, mysqlpass, mysqlhost, mysqldb))
 
-client.login(process.env.token);
-
 client.on('ready', () => {
-  console.log((`[BOT] Logged in as ${client.user.tag}!`).green);
+  console.log(`[BOT] Logged in as ${client.user.tag}!`);
 });
 var today;
 client.on("message", (msg) => {
-  var day = new Date().getDate; //get todays day
+  var day = new Date().getDate(); //get todays day
   if (today != day) {
     today = day
     //take 10% from all users
   }
   if (msg.content.startsWith("!")) {
-    var args = msg.content.split(" ")
-    var cmd = args[0].substring(1).toLowerCase()
-    var ment = msg.mentions
+    var args = msg.content.split(" ");
+    var cmd = args[0].substring(1).toLowerCase();
+    var ment = msg.mentions;
   }
-  if (cmd == "ping") {
-    msg.channel.send("pong!");
+  if (cmd == "!ping") {
+    msg.channel.send({embed:{
+    //"color": 0,
+    "timestamp": new Date(),
+    "fields": [
+      {
+        "name": ":ping_pong: Ping",
+        "value": "todo: Get Ping in MS"
+      }
+    ]
+  }});
   }
 });
+
+client.login(process.env.token);
