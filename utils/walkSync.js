9bc1de6b5adcab06e63c39f739493fadf30a7e7a
ImDaveead <ImDaveead@users.noreply.github.com>
2018-02-25 11:26:34 -0500

    fix

--- utils/walkSync.js
+++ utils/walkSync.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 let walkSync = (dir, filelist = []) => fs.readdirSync(dir)
  .map(file => fs.statSync(path.join(dir, file)).isDirectory()
 	  ? walkSync(path.join(dir, file), filelist)
