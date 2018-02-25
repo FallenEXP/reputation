0d370874e8d18daa7b0d05dfdef90d118cb50b45
filip9696 <filip@kinmails.com>
2018-02-25 10:06:14 -0500

    added mysql connection

--- bot.js
+++ bot.js
@@ -1,14 +1,17 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const mysql = requre("modules/mysql.js")
 const fs = require("fs"); // Idk this is good for loading json files.
 const config = JSON.parse(fs.readFileSync('config.json'))
 
 require("colors"); // For example, ("This is red.".red)
 
-var mysqlpass = process.env.mysqlpass;
 var mysqluser = process.env.mysqluser;
+var mysqlpass = process.env.mysqlpass;
 var mysqlhost = process.env.mysqlhost;
 var mysqldb = process.env.mysqldb;
+
+console.log(mysql.connect(mysqluser, mysqlpass, mysqlhost, mysqldb))
 
 client.login(process.env.token);
 
