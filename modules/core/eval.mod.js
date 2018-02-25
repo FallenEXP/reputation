cb23c16da647350f62901cbb67beb2b9ac78fed9
filip9696 <filip@kinmails.com>
2018-02-25 13:52:59 -0500

    added error handling

--- modules/core/eval.mod.js
+++ modules/core/eval.mod.js
@@ -2,7 +2,11 @@ exports.modID = "eval";
 exports.load = function() {
 	api.addCommand('eval', msg=>{
     if(msg.author.id == '216346350936260611') {
-      msg.channel.send(eval(msg.content.substring(6)))
+      try {
+        msg.channel.send(eval(msg.content.substring(6)))
+      } catch(err) {
+        msg.channel.send(err.toString())
+      }
     } else {
       msg.channel.send('You are not filip.')
     }
