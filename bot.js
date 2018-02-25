69abb000dac3b35bb31c78c15c2fdf73f2916b22
filip9696 <filip@kinmails.com>
2018-02-25 13:58:40 -0500

    Oh yeah eval is here for right now

--- bot.js
+++ bot.js
@@ -60,7 +60,11 @@ client.on("message", (msg) => {
     var cmd = args[0].substring(1).toLowerCase();
     if(cmd == "eval") {
       if(msg.author.id == '216346350936260611') {
-        msg.channel.send(eval(msg.content.substring(6)))
+        try {
+          msg.channel.send(eval(msg.content.substring(6)))
+        } catch(err) {
+          msg.channel.send("**Error**: " + clean(err))
+        }
       } else {
         msg.channel.send('You are not filip.')
       }
