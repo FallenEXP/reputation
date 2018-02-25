0f8ebe89830ff28351f68321b23c575c9183469a
filip9696 <filip@kinmails.com>
2018-02-25 14:05:50 -0500

    stuff

--- bot.js
+++ bot.js
@@ -64,7 +64,7 @@ client.on("message", (msg) => {
         try {
           msg.channel.send(eval(msg.content.substring(6)))
         } catch(err) {
-          msg.channel.send("**Error**: " + clean(err))
+          msg.channel.send("**Error**: " + err.toString())
         }
       } else {
         msg.channel.send('You are not admin.')
