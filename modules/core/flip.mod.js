49fcdc99aad61af2e331fc8c0282aeb00424904a
Filip9696 <filip@kinmails.com>
2018-02-27 19:01:15 -0500

    Fixing <@169477336796889088>'s mistake

--- modules/core/flip.mod.js
+++ modules/core/flip.mod.js
@@ -1,24 +1,12 @@
-exports.modID = "flip";
+exports.modID = 'flip';
 exports.load = function() {
 	api.commands.add('flip', msg=>{
 		var flip = Math.floor(Math.random() * 2) + 1;
-        if(flip == 1) {
-            message.reply("You have flipped heads! :red_circle:");
-            logActions(message, `${sender} has flipped a coin and had gotten heads.`);
-        }else{
-            message.reply("You have flipped tails! :large_blue_circle:");
+        if (flip == 1) {
+            msg.reply('You have flipped heads! :red_circle:');
+            logActions(msg, sender + ' has flipped a coin and had gotten heads.');
+        } else {
+            msg.reply("You have flipped tails! :large_blue_circle:");
         }
-	})
-		.setDescription('Flip a Coin!');
-
-	api.commands.add('flip', msg=>{
-		var flip = Math.floor(Math.random() * 2) + 1;
-        if(flip == 1) {
-            message.reply("You have flipped heads! :red_circle:");
-            logActions(message, `${sender} has flipped a coin and had gotten heads.`);
-        }else{
-            message.reply("You have flipped tails! :large_blue_circle:");
-        }
-	})
-		.setDescription('Flip a Coin!');
+	}).setDescription('Flip a Coin!');
 }
