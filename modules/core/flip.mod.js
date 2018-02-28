8722665f0e0a140fc971815502c98b81166321a4
Filip9696 <filip@kinmails.com>
2018-02-27 19:02:30 -0500

    Copy pasting is fails lol

--- modules/core/flip.mod.js
+++ modules/core/flip.mod.js
@@ -4,7 +4,6 @@ exports.load = function() {
 		var flip = Math.floor(Math.random() * 2) + 1;
         if (flip == 1) {
             msg.reply('You have flipped heads! :red_circle:');
-            logActions(msg, sender + ' has flipped a coin and had gotten heads.');
         } else {
             msg.reply("You have flipped tails! :large_blue_circle:");
         }
